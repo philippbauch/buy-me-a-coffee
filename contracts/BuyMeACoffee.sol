538fe7d19ed097e8d18f6f342ecce22bd9a60dda
Philipp Bauch <philippbauch@outlook.com>
2022-11-03 21:29:01 +0100

    Implement BuyMeACoffee smart contract

---
+++ contracts/BuyMeACoffee.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract BuyMeACoffee {
+    event NewMemo(
+        address indexed from,
+        uint256 timestamp,
+        string name,
+        string message
+    );
+
+    struct Memo {
+        address from;
+        uint256 timestamp;
+        string name;
+        string message;
+    }
+
+    Memo[] memos;
+
+    // Address if the contract deployer
+    address payable owner;
+
+    // Constructor is run only when the contract is deployed
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    /**
+     * @dev buy a coffee for contract owner
+     * @param _name name of the coffee buyer
+     * @param _message a nice message from the coffee buyer
+     */
+    function buyCoffee(string memory _name, string memory _message)
+        public
+        payable
+    {
+        require(msg.value > 0, "can't buy coffee with 0 eth");
+
+        // Add the memo to the storage
+        memos.push(Memo(msg.sender, block.timestamp, _name, _message));
+
+        // Emit a log event when a new memo is created
+        emit NewMemo(msg.sender, block.timestamp, _name, _message);
+    }
+
+    /**
+     * @dev send the entire balance stored in this contract to the owner
+     */
+    function withdrawTips() public {
+        require(owner.send(address(this).balance));
+    }
+
+    /**
+     * @dev retrieve all the memos received and stored on the blockchain
+     */
+    function getMemos() public view returns (Memo[] memory) {
+        return memos;
+    }
+}
